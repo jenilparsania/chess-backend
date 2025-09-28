6fb5e3e65c57bb673178c9cf7042cb0a90c7c816
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2025-09-28 17:20:13 -0300

    Validating moves by chess.js

--- src/Game.ts
+++ src/Game.ts
@@ -1,23 +1,38 @@
 import { WebSocket } from "ws";
-
+import { Chess } from "chess.js";
 export class Game{
     public player1 : WebSocket;
     public player2 : WebSocket;
-    public board:string;
+    public board:Chess;
     public moves:string[];
     public startTime:Date;
 
     constructor(player1:WebSocket,player2:WebSocket){
         this.player1 = player1;
         this.player2 = player2;
-        this.board = "";
+        this.board = new Chess();
         this.moves = [];
         this.startTime = new Date();
 
     }
 
-    makeMove(socket : WebSocket,move : String){
+    makeMove(socket : WebSocket,move : {
+        from:string;
+        to:string;
+    }){
         //validation here
+        if(this.board.moves.length % 2 === 0 && socket !== this.player1){
+            return
+        }
+
+        if(this.board.moves.length % 2 === 1 && socket !== this.player2){
+            return;
+        }
+        try {
+            this.board.move(move);
+        } catch (error) {
+            return
+        }
         //is it this user move
         //is the move valid
         //update the board
