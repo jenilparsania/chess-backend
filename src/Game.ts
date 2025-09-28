7a78dbd06ec4b9d19d4a3cf95f903037d1c60f3d
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2025-09-27 21:23:11 -0300

    Game class

---
+++ src/Game.ts
@@ -0,0 +1,18 @@
+import { WebSocket } from "ws";
+
+export class Game{
+    public player1 : WebSocket;
+    public player2 : WebSocket;
+    public board:string;
+    public moves:string[];
+    public startTime:Date;
+
+    constructor(player1:WebSocket,player2:WebSocket){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = "";
+        this.moves = [];
+        this.startTime = new Date();
+
+    }
+}
--- src/GameManager.ts
+++ src/GameManager.ts
@@ -1,19 +1,22 @@
 import WebSocket from "ws";
-import { INIT_GAME } from "./messages";
+import { INIT_GAME, MOVE } from "./messages";
+import { Game } from "./Game";
 
 // User,Game
 export class GameManager{
     private games:Game[];
-    private pendingUser:WebSocket;
+    private pendingUser:WebSocket | null;
     private users:WebSocket[];
 
     constructor(){
         this.games = [];
+        this.pendingUser = null;
+        this.users = [];
     }
 
     addUser(socket:WebSocket){
         this.users.push(socket);
-        this.addHandler();
+        this.addHandler(socket);
     }
 
     removeUser(socket:WebSocket){
@@ -31,8 +34,18 @@ export class GameManager{
             if(message.type === INIT_GAME){
                 if(this.pendingUser){
                     // Start a game
+                    const game = new Game(this.pendingUser,socket);
+                    this.games.push(game);
+                    this.pendingUser = null;
                 }else{
                     this.pendingUser = socket;
+                }
+            }
+
+            if(message.type === MOVE){
+                const game = this.games.find(game => game.player1===socket || game.player2 === socket);
+                if(game){
+                    game.makeMove(socket,message.move)
                 }
             }
         })
